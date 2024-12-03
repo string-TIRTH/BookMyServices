ba480499d1e48a25f4988fd8801d9afab9917623
Tirth Prajapati <66546710+string-TIRTH@users.noreply.github.com>
2024-12-03 18:42:57 +0530

    Update cloudinary.js

--- Backend/src/config/cloudinary.js
+++ Backend/src/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary');
 
 cloudinary.v2.config({
-  cloud_name: 'dexuvfg93',
-  api_key: '971153597481448',
-  api_secret: 'V6m7v0se7iOmJivrZAR8209tLyE',
+  cloud_name: 'cloudname',
+  api_key: 'apikey',
+  api_secret: 'apisecret',
   secure: true,
 });
 
